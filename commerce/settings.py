2890e3892e299c7f08a81a91a71555e88bae4b9d
Alfio Martini <alfio.martini@gmail.com>
2020-09-14 02:29:53 -0300

    Secret key in settings saved in env variable

--- commerce/settings.py
+++ commerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5(6gb7=fdjsjbir#izgz=^sqh0dl)fhc=lvj@bp%q!rey0&efc'
+SECRET_KEY = os.environ.get('AUCTION_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
