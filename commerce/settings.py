dbd7d54cbc28dffeecf710b48feea315e7c30ef0
Alfio Martini <alfio.martini@gmail.com>
2020-11-13 23:51:58 -0300

    Updated static_files with STATICFILES_FINDERS

--- commerce/settings.py
+++ commerce/settings.py
@@ -144,6 +144,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/media/images',
+    'media/images',
 ]
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
