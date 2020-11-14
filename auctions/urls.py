c9dc0f6d358ab78b5167ff78c7c0e22f0473a529
Alfio Martini <alfio.martini@gmail.com>
2020-11-14 00:19:08 -0300

    Updated static settings in urls.py

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -9,12 +11,18 @@ urlpatterns = [
     path("listing/<int:listing_id>", views.list_detail, name='listing'),
     path('listing/new', views.create_listing, name='create_listing'),
     path("whatchlist/list", views.whatchlist, name='whatchlist'),
-    path("whatchlist/add/<int:listing_id>", views.whatchlist_add, name='whatch_add'),
-    path("whatchlist/del/<int:listing_id>", views.whatchlist_del, name='whatch_del'),
+    path("whatchlist/add/<int:listing_id>",
+         views.whatchlist_add, name='whatch_add'),
+    path("whatchlist/del/<int:listing_id>",
+         views.whatchlist_del, name='whatch_del'),
     path('comment/add/<int:listing_id>', views.add_comment, name='add_comment'),
     path('bid/place/<int:listing_id>', views.place_bid, name='place_bid'),
     path('bid/close/<int:listing_id>', views.close_auction, name='close_auction'),
-    path('category', views.categories, name = 'categories'),
-    path('category/<int:category_id>', views.category_listings, name='listings_in_cat'),
+    path('category', views.categories, name='categories'),
+    path('category/<int:category_id>',
+         views.category_listings, name='listings_in_cat'),
     path('readme', views.readme, name='readme')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# this will help to access your media folder.
